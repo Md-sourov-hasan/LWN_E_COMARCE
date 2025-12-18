9bae07bc7747ba6e1754b9c67e64807b08b5759a
Md Sourov Hasan <mdsourobhasan2552@gmail.com>
2025-12-18 09:19:24 +0600

    all_colors

---
+++ lib/const/all_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AllColors {
+  static final primaryColor=Color(0xff0066ff);
+  static final secondaryColor=Color(0xff9933ff);
+  static final whiteColor=Color(0xffffffff);
+  static final blackColor=Color(0xff151313);
+
+}
